c8ec44e0c05faadea91058f8a51a5755facc14c1
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2026-05-23 12:23:07 +0200

    Move .net maui demo to more specific folder and add demo using .net console app

---
+++ src/Demo/ServiceAutoInjector.Demo.ConsoleApp/DependencyInjectionExtension.cs
@@ -0,0 +1,32 @@
+﻿using ServiceAutoInjector.Interfaces;
+using ServiceAutoInjector.Logic;
+using ServiceAutoInjector;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceAutoInjector.Demo.Extensions
+{
+    public static class DependencyInjectionExtension
+    {
+        extension(IServiceCollection services)
+        {
+            public IServiceCollection AddRequiredManualyServices()
+            {
+                services.AddTransient<IBookService, BookService>();
+                services.AddTransient<IAuthorService, AuthorService>();
+                services.AddTransient<IAdminService, AdminService>();
+                services.AddTransient<IFurnitureService, FurnitureService>();
+                services.AddTransient<IAnimalService, AnimalService>();
+
+                return services;
+            }
+
+            public IServiceCollection AddRequiredServicesByServiceAutoInjector()
+            {
+                services.AddClassesToDependencyInjection(typeof(IService));
+                services.AddClassesToDependencyInjection(typeof(IOtherService));
+
+                return services;
+            }
+        }
+    }
+}
